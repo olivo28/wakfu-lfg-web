6ca4ae409693790d930b92b5d0d7f355ee3680fa
Olivo28 <emi24_35@hotmail.com>
2026-02-23 21:04:41 -0400

    aaaaa

--- js/core/seo.js
+++ js/core/seo.js
@@ -9,8 +9,8 @@ export const SEO = {
     _defaultTags: {
         title: 'Wakfu LFG - Buscador de Grupos',
         description: 'Encuentra grupo para mazmorras en Wakfu de forma rápida y sencilla.',
-        image: `https://wakfu.pcnetfs.moe${CONFIG.BASE_PATH}/assets/ui/og_image.jpg`,
-        url: `https://wakfu.pcnetfs.moe${CONFIG.BASE_PATH}`,
+        image: `${CONFIG.BASE_PATH}/assets/ui/og_image.jpg`,
+        url: `${CONFIG.BASE_PATH}/`,
         keywords: 'wakfu, lfg, groups, dungeons, mmo, tactical',
         author: 'Antigravity'
     },
